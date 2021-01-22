9dc46770c3479493f3953533309ad599ec73fc44
Peter Roth <p.roth@fo-zuerisee.ch>
2021-01-22 14:24:16 +0100

    cleanup

---
+++ maps/tilesets/fantasy.tsx
@@ -0,0 +1,29 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="fantasy" tilewidth="32" tileheight="32" tilecount="1064" columns="8">
+ <image source="../pub-map/fantasy.png" trans="ff00ff" width="256" height="4256"/>
+ <tile id="708">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="716">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="746">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="747">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="748">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
